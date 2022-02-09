6673e26c79b648f6327fc928350b96daed0a7bb8
Chrispy98 <45731625+Chrispy98@users.noreply.github.com>
2022-02-09 13:45:10 +0100

    Add files via upload

--- javascript/webfuhrer/js/funciones3.js
+++ javascript/webfuhrer/js/funciones3.js
@@ -1,9 +1,9 @@
 function cambiarEstado()
 {
 	let nombre_clase=document.getElementById("parrafo_lorem").className;
-	if (nombre_clase=="mostrado")
-		{document.getElementById("parrafo_lorem").className="oculto";}
+	if (nombre_clase=="verde")
+		{document.getElementById("parrafo_lorem").className="rojo";}
 	else
-		{document.getElementById("parrafo_lorem").className="mostrado";}
+		{document.getElementById("parrafo_lorem").className="verde";}
 	
 }
