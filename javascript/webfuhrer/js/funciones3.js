2d010c84336db9d099812a68522020a9a0181562
Chrispy98 <45731625+Chrispy98@users.noreply.github.com>
2022-02-09 14:00:15 +0100

    Add files via upload

--- javascript/webfuhrer/js/funciones3.js
+++ javascript/webfuhrer/js/funciones3.js
@@ -2,8 +2,10 @@ function cambiarEstado()
 {
 	let nombre_clase=document.getElementById("parrafo_lorem").className;
 	if (nombre_clase=="verde")
-		{document.getElementById("parrafo_lorem").className="rojo";}
+		{document.getElementById("parrafo_lorem").className="rojo";
+		document.getElementById("parrafo_lorem").innerHTML="ROJO"
+}
 	else
-		{document.getElementById("parrafo_lorem").className="verde";}
-	
+		{document.getElementById("parrafo_lorem").className="verde";
+		document.getElementById("parrafo_lorem").innerHTML="VERDE"}
 }
